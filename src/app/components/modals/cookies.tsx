7da9d37b52371eaa5326ff9b897dcefed658372f
v.thmrv <thmrvwork@gmail.com>
2024-03-22 23:50:03 +0300

    fixes

--- src/app/components/modals/cookies.tsx
+++ src/app/components/modals/cookies.tsx
@@ -6,9 +6,9 @@ import Button from "../button";
 
 export default function Cookies({ content, buttonText, buttonTextCancel }: { content: string, buttonText: string, buttonTextCancel: string }) {
     return (<Suspense fallback={<Loading />}>
-        <div className="modal cookies container flex bg-white w-max p-4 rounded-md max-w-xl flex-column gap-4 items-center">
+        <div className="modal cookies container flex flex-col bg-white w-max p-4 rounded-md max-w-xl flex-column gap-4 items-center">
             <p className="text-sm font-normal">{content}</p>
-            <div className="button-group cookies flex gap-2">
+            <div className="button-group cookies flex justify-between w-full gap-2">
                 <Button onClick={(e) => { document != null ? (document.querySelector('.modal.cookies') as HTMLElement).style.display = 'none' : Error('Document is NULL.') }} downSizing={true} text={buttonText} />
                 <Button onClick={(e) => { document != null ? (document.querySelector('.modal.cookies') as HTMLElement).style.display = 'none' : Error('Document is NULL.') }} downSizing={true} text={buttonTextCancel} fillcolor="var(--bluegrey)" />
             </div>
