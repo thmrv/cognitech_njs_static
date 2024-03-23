b9d0badafddac5d91e1451068071b15772ce7053
v.thmrv <thmrvwork@gmail.com>
2024-03-23 03:11:40 +0300

    update preloader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 
 function Loading() {
     return (
-        <div className="preloader-suspense w-screen h-screen flex justify-center items-center black-fill">
+        <div className="preloader-suspense w-screen h-screen flex justify-center items-center bluegrey-fill">
             <div className="preloader">
                 <div></div><div></div>
             </div>
